29b819e45f677f777d4c2cf3d59ff6dde6011aa7
yaroslav154 <66218411+yaroslav154@users.noreply.github.com>
2022-07-20 21:41:24 -0400

    Modificacion en la parte de loginpersonal

--- personal/vista/loginpersonal.php
+++ personal/vista/loginpersonal.php
@@ -22,7 +22,7 @@
                         <div class="col-lg-5">
                             <div class="card shadow-lg border-0 rounded-lg mt-5">
                                 <div class="card-header">
-                                    <h3 class="text-center font-weight-light my-4">ADMINISTRADOR</h3>
+                                    <h3 class="text-center font-weight-light my-4">PERSONAL</h3>
                                 </div>
                                 <div class="card-body">
                                     <form action="../modelo/verificarUsuario2.php" method="POST">
